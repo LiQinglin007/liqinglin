26781bc0feeb07d851fbe635c5fc74a7b7ac1b91
hanzhi_dell <531192557@qq.com>
2018-06-27 08:43:52 +0800

    sql文件

--- editorMi.sql
+++ editorMi.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50512
 File Encoding         : 65001
 
-Date: 2018-06-14 18:17:20
+Date: 2018-06-27 08:43:28
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,11 +20,13 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `banner`;
 CREATE TABLE `banner` (
-  `banner_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '图片id',
+  `banner_id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '图片id',
   `banner_url` varchar(200) NOT NULL COMMENT '图片地址',
-  `banner_weight` smallint(2) unsigned zerofill NOT NULL COMMENT '权重  数字越大越在前边显示',
+  `banner_weight` smallint(2) NOT NULL DEFAULT '0' COMMENT '权重  数字越大越在前边显示',
+  `banner_web_url` varchar(200) DEFAULT NULL COMMENT '外部连接',
+  `banner_del` smallint(1) DEFAULT '0' COMMENT '是否被删除  0：没删除  1：被删除了',
   PRIMARY KEY (`banner_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for collection
@@ -39,6 +41,26 @@ CREATE TABLE `collection` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
+-- Table structure for commodity
+-- ----------------------------
+DROP TABLE IF EXISTS `commodity`;
+CREATE TABLE `commodity` (
+  `commodity_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '商品id',
+  `studio_id` int(11) DEFAULT NULL COMMENT '工作室ID',
+  `commodity_name` varchar(50) DEFAULT NULL COMMENT '商品名称',
+  `commodity_pic` varchar(30) DEFAULT NULL COMMENT '商品图片',
+  `commodity_pics` varchar(300) DEFAULT NULL COMMENT '商品详情图片',
+  `commodity_original_price` float DEFAULT NULL COMMENT '商品原价',
+  `commodity_present_price` float DEFAULT NULL COMMENT '商品现价',
+  `commodity_type` smallint(1) DEFAULT NULL COMMENT '服务类型(1:查重、2:降重、3:速审)',
+  `commodity_monthly_sales` int(11) DEFAULT '0' COMMENT '月销量',
+  `commodity_collection_quantity` int(11) DEFAULT '0' COMMENT '收藏数量',
+  `commodity_hot` smallint(1) DEFAULT '0' COMMENT '是否为热门服务  0:不是  1：热门',
+  `commodity_del` smallint(1) NOT NULL DEFAULT '0' COMMENT '是否被删除  0：不被删除  1：被删除了',
+  PRIMARY KEY (`commodity_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
 -- Table structure for evaluate
 -- ----------------------------
 DROP TABLE IF EXISTS `evaluate`;
@@ -47,7 +69,7 @@ CREATE TABLE `evaluate` (
   `order_id` int(11) DEFAULT NULL COMMENT '工作室ID',
   `evaluate_conetnt` varchar(100) DEFAULT NULL COMMENT '评价内容',
   `evaluate_time` datetime DEFAULT NULL COMMENT '评论时间',
-  `evaluate_score` tinyint(1) NOT NULL DEFAULT '0' COMMENT '评分',
+  `evaluate_score` smallint(1) NOT NULL DEFAULT '0' COMMENT '评分  1:好评  2：差评',
   PRIMARY KEY (`evaluate_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -69,12 +91,13 @@ CREATE TABLE `feedback` (
 -- ----------------------------
 DROP TABLE IF EXISTS `notice`;
 CREATE TABLE `notice` (
-  `notice_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '公告ID',
-  `notice_title` varchar(30) NOT NULL COMMENT '公告标题',
+  `notice_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `notice_title` varchar(20) NOT NULL COMMENT '公告标题',
   `notice_content` varchar(500) NOT NULL COMMENT '公告内容',
-  `notice_time` datetime NOT NULL COMMENT '公告时间',
+  `notice_time` datetime DEFAULT NULL,
+  `notice_del` smallint(1) DEFAULT '0' COMMENT '是否被删除  0：没删除  1：被删除了',
   PRIMARY KEY (`notice_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for order
@@ -106,35 +129,11 @@ DROP TABLE IF EXISTS `resultfile`;
 CREATE TABLE `resultfile` (
   `file_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `file_name` varchar(30) DEFAULT NULL COMMENT '文件名称',
-  `subject_id` int(10) DEFAULT NULL COMMENT '学科方向',
   `author` varchar(10) NOT NULL COMMENT '作者',
-  `author_title` varchar(10) DEFAULT NULL COMMENT '作者职称',
   `author_degree` varchar(10) DEFAULT NULL COMMENT '作者学历',
   `downloadurl` varchar(200) DEFAULT NULL COMMENT '下载地址',
   `see_time` datetime DEFAULT NULL COMMENT '见刊时间',
   PRIMARY KEY (`file_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Table structure for servlet
--- ----------------------------
-DROP TABLE IF EXISTS `servlet`;
-CREATE TABLE `servlet` (
-  `servlet_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '商品id',
-  `studio_id` int(11) DEFAULT NULL COMMENT '工作室ID',
-  `servlet_name` varchar(50) DEFAULT NULL COMMENT '商品名称',
-  `servlet_pic` varchar(30) DEFAULT NULL COMMENT '商品图片',
-  `servlet_pics` varchar(300) DEFAULT NULL COMMENT '商品详情图片',
-  `servlet_original_price` float DEFAULT NULL COMMENT '商品原价',
-  `servlet_present_price` float DEFAULT NULL COMMENT '商品现价',
-  `servlet_type` smallint(1) DEFAULT NULL COMMENT '服务类型(1:查重、2:降重、3:速审)',
-  `servlet_monthly_sales` int(11) DEFAULT '0' COMMENT '月销量',
-  `servlet_collection_quantity` int(11) DEFAULT '0' COMMENT '收藏数量',
-  `servlet_type_little` smallint(3) DEFAULT NULL COMMENT '服务类型(1:期刊职称论文查重、2:学位分段、3:本科学位、4:硕博论文)',
-  `servlet_hot` smallint(1) DEFAULT '0' COMMENT '是否为热门服务',
-  `servlet_level` varchar(100) DEFAULT NULL COMMENT '刊物级别',
-  `servlet_subject_id` varchar(100) DEFAULT NULL COMMENT '学科范围',
-  PRIMARY KEY (`servlet_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -147,7 +146,7 @@ CREATE TABLE `statistics` (
   `statistics_type` smallint(1) DEFAULT NULL COMMENT '统计类型（0:工作室、1:服务）',
   `content_id` int(11) DEFAULT NULL COMMENT '工作室的id  或者是服务的id',
   `content_sum_number` int(11) DEFAULT NULL COMMENT '工作室/服务销售总数量',
-  `content_sum_score` int(11) DEFAULT NULL COMMENT '当前工作室/服务的总分数',
+  `content_sum_score` int(11) DEFAULT NULL COMMENT '当前工作室/服务的好评数',
   `content_sum_number_month` int(11) DEFAULT NULL COMMENT '当前工作室/服务的当前月累积销量',
   PRIMARY KEY (`statistics_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -161,25 +160,28 @@ CREATE TABLE `studio` (
   `studio_name` varchar(30) DEFAULT NULL COMMENT '商铺名称',
   `studio_pic` varchar(30) DEFAULT NULL COMMENT '工作室图片',
   `studio_money` float DEFAULT '0' COMMENT '诚信保证金',
-  `studio_reputation_value` float DEFAULT '0' COMMENT '信誉值',
   `studio_phone` varchar(11) DEFAULT NULL COMMENT '工作室电话',
   `studio_monthly_sales` int(10) DEFAULT '0' COMMENT '月销量',
   `studio_QQ` varchar(15) DEFAULT NULL COMMENT '商铺QQ',
   `studio_briefIntroduction` varchar(100) DEFAULT NULL COMMENT '商铺简介',
   `studio_collection_nmuber` int(11) DEFAULT '0' COMMENT '收藏数量',
+  `studio_del` smallint(1) NOT NULL DEFAULT '0' COMMENT '是否被删除  0：没删除  1：被删除了',
+  `system_userid` int(10) DEFAULT NULL COMMENT '系统用户id',
   PRIMARY KEY (`studio_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for subject
+-- Table structure for system
 -- ----------------------------
-DROP TABLE IF EXISTS `subject`;
-CREATE TABLE `subject` (
-  `subject_id` smallint(5) NOT NULL AUTO_INCREMENT,
-  `subject_number` varchar(20) NOT NULL COMMENT '学科编号',
-  `subject_name` varchar(20) NOT NULL COMMENT '学科名称',
-  PRIMARY KEY (`subject_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `system`;
+CREATE TABLE `system` (
+  `system_userid` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `system_user_loginname` varchar(255) DEFAULT NULL,
+  `system_user_password` varchar(255) DEFAULT NULL,
+  `system_user_type` smallint(2) DEFAULT NULL COMMENT '用户类型  1：超级管理员  2：工作室',
+  `system_user_del` smallint(1) DEFAULT '0' COMMENT '是否被删除  0：没删除  1：被删除了',
+  PRIMARY KEY (`system_userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for user
@@ -187,24 +189,17 @@ CREATE TABLE `subject` (
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id',
-  `user_name` varchar(15) DEFAULT NULL COMMENT '用户昵称',
-  `user_password` varchar(12) NOT NULL COMMENT '密码',
+  `user_name` varchar(100) DEFAULT NULL COMMENT '用户昵称',
+  `user_password` varchar(50) NOT NULL COMMENT '密码',
   `user_pic` varchar(100) DEFAULT NULL COMMENT '头像',
   `user_balance` float DEFAULT '0' COMMENT '账户余额',
   `user_phone` varchar(15) NOT NULL DEFAULT '15284224244' COMMENT '电话（登陆名称）',
-  `user_realName` tinyint(1) DEFAULT '0' COMMENT '是否实名认证',
   `user_school` varchar(20) DEFAULT NULL COMMENT '学校',
   `user_job` varchar(10) DEFAULT NULL COMMENT '职业',
   `user_education` varchar(5) DEFAULT NULL COMMENT '学历',
-  `user_title` varchar(15) DEFAULT NULL COMMENT '职称',
-  `user_withdrawals_account` varchar(20) DEFAULT NULL COMMENT '提现账号(支付宝)',
-  `user_withdrawals_name` varchar(20) DEFAULT NULL COMMENT '提现名(支付宝)',
-  `userIdPic1` varchar(100) DEFAULT NULL COMMENT '身份证正面照片',
-  `userIdPic2` varchar(100) DEFAULT NULL COMMENT '身份证反面照片',
-  `userIdPic3` varchar(100) DEFAULT NULL COMMENT '手持身份证照片',
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `userPhone` (`user_phone`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for userfile
